4e1f677bfe25b32cb504a89762f48fcc046c983c
saifullashaik1 <ssaifullashaik53@gmail.com>
2024-09-12 16:18:54 +0530

    Update project

---
+++ javabasics/ArrayExample.java
@@ -0,0 +1,58 @@
+package javabasics;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        // Declaring an array of integers
+        int[] numbers = new int[5];  // Array size is 5
+
+// Initializing values
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+
+
+// Accessing elements
+        System.out.println(numbers[0]);  // Outputs 10
+        System.out.println(numbers[3]);  // Outputs 40
+
+// Modifying elements
+        numbers[2] = 100;  // Changing value at index 2 to 100
+        System.out.println(numbers[2]);  // Outputs 10
+
+// ;ength of an array
+
+        System.out.println(numbers.length);  // Outputs 5
+
+// iterating over arrays
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);  // Outputs each element
+        }
+
+        // multidimensional array
+
+        // Declaring and initializing a 2D array
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+// Accessing elements
+        System.out.println(matrix[0][1]);  // Outputs 2 (element at row 0, column 1)
+        System.out.println(matrix[2][2]);  // Outputs 9 (element at row 2, column 2)
+
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }}
---
+++ javabasics/readme2.md
@@ -0,0 +1,18 @@
+# day 2
+# Array:
+
+* In Java, an array is a collection of elements of the same data type, stored in a contiguous memory location.
+* Arrays are useful for storing multiple values in a single variable and can be used with both primitive data types (e.g., int, char, float) and reference types (e.g., String, objects).
+* Declaring and Initializing Arrays:
+  1.Declaration: An array must first be declared, specifying its data type.
+  2.Instantiation: The array is then instantiated using the new keyword.
+  3.Initialization: Finally, the array can be initialized with values.
+
+## Multidimensional Arrays :
+* Java allows the creation of arrays with more than one dimension, such as 2D arrays (arrays of arrays).
+
+## Limitations of Arrays:
+* Fixed Size: The size of an array is fixed at the time of creation, so you cannot resize it dynamically.
+* Homogeneous Data: Arrays can only store elements of the same type.
+* Limited Built-in Functions: Compared to collections (like ArrayList), arrays have fewer utility methods.
+
